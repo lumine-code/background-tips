2cced559943909d4c30df8d29fc63840f4a962a4
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2026-08-30 20:05:57 +0200

    Remove detached window support

--- spec/background-tips-spec.js
+++ spec/background-tips-spec.js
@@ -15,7 +15,7 @@ describe("BackgroundTips", () => {
 
   describe("when the package is activated when there is only one pane", () => {
     beforeEach(() => {
-      expect(lumine.workspace.getCenter().getTiledPanes().length).toBe(1);
+      expect(lumine.workspace.getCenter().getPanes().length).toBe(1);
     });
 
     describe("when the pane is empty", () => {
@@ -26,17 +26,6 @@ describe("BackgroundTips", () => {
         expect(backgroundTipsView.element.parentNode).toBeFalsy();
         advanceClock(backgroundTipsView.startDelay + 1);
         expect(backgroundTipsView.element.parentNode).toBeTruthy();
-      });
-
-      it("uses the empty tiled pane even when another center pane is detached", async () => {
-        const center = lumine.workspace.getCenter();
-        const tiledPane = center.getTiledPanes()[0];
-        spyOn(center, "getPanes").and.returnValue([tiledPane, { isDetached: () => true }]);
-
-        const backgroundTipsView = await activatePackage();
-        advanceClock(backgroundTipsView.startDelay + 1);
-
-        expect(backgroundTipsView.element.parentNode).toBe(tiledPane.getElement());
       });
     });
 
@@ -65,7 +54,7 @@ describe("BackgroundTips", () => {
   describe("when the package is activated when there are multiple panes", () => {
     beforeEach(() => {
       lumine.workspace.getActivePane().splitRight();
-      expect(lumine.workspace.getCenter().getTiledPanes().length).toBe(2);
+      expect(lumine.workspace.getCenter().getPanes().length).toBe(2);
     });
 
     it("does not attach the view", async () => {
@@ -94,7 +83,7 @@ describe("BackgroundTips", () => {
     let backgroundTipsView;
 
     beforeEach(async () => {
-      expect(lumine.workspace.getCenter().getTiledPanes().length).toBe(1);
+      expect(lumine.workspace.getCenter().getPanes().length).toBe(1);
 
       backgroundTipsView = await activatePackage();
       advanceClock(backgroundTipsView.startDelay);
